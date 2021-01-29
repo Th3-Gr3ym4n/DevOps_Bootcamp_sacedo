e9fab19c7945899f768c2dbe33cfe16850ef3bc2
Th3-Gr3ym4n <greyman@elsombrerogris.com>
2021-01-29 22:26:15 +0100

    adding argument condition scenario

--- modulo1/mo1_opt_bash.sh
+++ modulo1/mo1_opt_bash.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-curl -o fileweb.txt https://www.shodan.io/
-grep -in "$1" fileweb.txt
+var1=$1
+if [[ "$var1! fileweb1.txt ]]; then
+
+  curl -o fileweb.txt https://www.shodan.io/
+  grep -in "$var1" fileweb.txt
+else
+  echo "Introduce texto a buscar"
+fi
